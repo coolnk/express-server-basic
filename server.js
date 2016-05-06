822584e699256907d05eaba1a18fb9a732819924
Nirjal Khadka <nirjal@gmail.com>
2016-05-06 11:28:41 +0200

    fixed bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
-app.use(express.static(__dirname-'/public'));
+app.use(express.static(__dirname+'/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
