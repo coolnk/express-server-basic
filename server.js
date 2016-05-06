817581fcb85442fd3ad49ffd2426707a83ada3f3
Nirjal Khadka <nirjal@gmail.com>
2016-05-06 12:50:13 +0200

    Added index and started the server

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 
-
 app.use(express.static(__dirname+'/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type:'applicatin/vnd.api+json'}));
 
+app.get('*', function(req,res){
+    res.sendfile('.public/index.html');
+})
 
 app.listen(7070);
 console.log("App listening on 7070");
